bbbf3b38a415e180818f9cfbc388da82da38928c
Noah Harris <mail@gastrodon.io>
2021-01-11 02:25:10 -0500

    Add register tests
    Add tests for functions that register routes to handle

--- groudon_test.go
+++ groudon_test.go
@@ -1,344 +1,110 @@
 package groudon
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"os"
-	"regexp"
 	"testing"
 )
 
-var (
-	keep_catch map[int]map[string]interface{}
-	blank      *http.Request = new(http.Request)
-)
-
-func silent(_ *http.Request) (_ int, _ map[string]interface{}, _ error) {
-	return
-}
-
-func ohno(_ *http.Request) (_ int, r_map map[string]interface{}, _ error) {
-	r_map = map[string]interface{}{"thing": make(chan int)}
-	return
-}
-
-func ahshit(_ *http.Request) (_ int, _ map[string]interface{}, err error) {
-	err = fmt.Errorf("oopsies")
-	return
-}
-
-func pretendler(_ *http.Request) (code int, r_map map[string]interface{}, _ error) {
-	code = 200
-	r_map = map[string]interface{}{"hello": "world"}
-	return
-}
-
-func altler(_ *http.Request) (code int, _ map[string]interface{}, _ error) {
-	code = 400
-	return
-}
-
-func donotpass(request *http.Request) (_ *http.Request, pass bool, code int, _ map[string]interface{}, _ error) {
-	pass = false
-	code = 409
-	return
-}
-
-func defaultable(_ *http.Request) (code int, _ map[string]interface{}, _ error) {
-	code = 300
-	return
-}
-
 func restore() {
-	stored_expressions = make(map[string]*regexp.Regexp)
-	path_handlers = make(map[*regexp.Regexp]*MethodMap)
-	middleware_handlers = make([]func(*http.Request) (*http.Request, bool, int, map[string]interface{}, error), 0)
-	default_route = defaultRoute
-	default_method = defaultMethod
-	catchers = keep_catch
+	handlers = make([]Handler, 0)
+	middleware = make([]Middleware, 0)
+}
+
+func request(method, path string) (made *http.Request) {
+	made, _ = http.NewRequest(method, "http://localhost"+path, nil)
 	return
+}
+
+func blank() (made *http.Request) {
+	made = request("GET", "")
+	return
+}
+
+func handlerSays(message string) (handler FuncHandler) {
+	handler = func(_ *http.Request) (_ int, data map[string]interface{}, _ error) {
+		data = map[string]interface{}{"message": message}
+		return
+	}
+
+	return
+}
+
+func middlewareSays(message string) (ware FuncMiddleware) {
+	ware = func(_ *http.Request) (_ *http.Request, _ bool, _ int, data map[string]interface{}, _ error) {
+		data = map[string]interface{}{"message": message}
+		return
+	}
+
+	return
+}
+
+func funcHandlerOk(handler FuncHandler, message string, test *testing.T) {
+	var body map[string]interface{}
+	var err error
+	if _, body, err = handler(blank()); err != nil {
+		test.Fatal(err)
+	}
+
+	var bodyMessage string
+	var ok bool
+	if bodyMessage, ok = body["message"].(string); !ok {
+		test.Fatalf("body %#v has no string message", body)
+	}
+
+	if bodyMessage != message {
+		test.Fatalf("message %s != wanted %s", bodyMessage, message)
+	}
+}
+
+func funcMiddlewareOk(ware FuncMiddleware, message string, test *testing.T) {
+	var body map[string]interface{}
+	var err error
+	if _, _, _, body, err = ware(blank()); err != nil {
+		test.Fatal(err)
+	}
+
+	var bodyMessage string
+	var ok bool
+	if bodyMessage, ok = body["message"].(string); !ok {
+		test.Fatalf("body %#v has no string message", body)
+	}
+
+	if bodyMessage != message {
+		test.Fatalf("message %s != wanted %s", bodyMessage, message)
+	}
+}
+
+func handlerOk(handler Handler, request *http.Request, message string, test *testing.T) {
+	if handler.Method != request.Method {
+		test.Fatalf("method incorrect, %s != %s", handler.Method, request.Method)
+	}
+
+	if !handler.Route.MatchString(request.URL.Path) {
+		test.Fatalf("route incorrect, %s doesn't match %s",
+			handler.Route.String(),
+			request.URL.Path,
+		)
+	}
+
+	funcHandlerOk(handler.Func, message, test)
+}
+
+func middlewareOk(ware Middleware, request *http.Request, message string, test *testing.T) {
+	if ware.Method != request.Method {
+		test.Fatalf("method incorrect, %s != %s", ware.Method, request.Method)
+	}
+
+	if !ware.Route.MatchString(request.URL.Path) {
+		test.Fatalf("route incorrect, %s doesn't match %s",
+			ware.Route.String(),
+			request.URL.Path,
+		)
+	}
+
+	funcMiddlewareOk(ware.Func, message, test)
 }
 
 func TestMain(main *testing.M) {
-	keep_catch = make(map[int]map[string]interface{}, len(catchers))
-
-	var code int
-	var catch map[string]interface{}
-	for code, catch = range catchers {
-		keep_catch[code] = catch
-	}
-
 	os.Exit(main.Run())
-}
-
-// I used the test to test the test
-func Test_restore(test *testing.T) {
-	RegisterDefaultMethod(defaultable)
-
-	var code int
-	if code, _, _ = default_method(blank); code != 300 {
-		test.Errorf("default_method got code %d", code)
-	}
-
-	catchers[400] = map[string]interface{}{
-		"error": "oopsies",
-	}
-
-	restore()
-
-	if code, _, _ = default_method(blank); code != 405 {
-		test.Errorf("default_method was not restored, got code %d", code)
-	}
-
-	if catchers[400]["error"].(string) != "bad_request" {
-		test.Errorf("catchers was not restored, got 400: %#v", catchers[400])
-	}
-}
-
-func Test_RegisterHandler(test *testing.T) {
-	defer restore()
-
-	RegisterHandler("GET", "/", pretendler)
-
-	var re_pointer *regexp.Regexp = getRegexPointer("/")
-
-	var code int
-	code, _, _ = (*path_handlers[re_pointer])["GET"](blank)
-
-	if code != 200 {
-		test.Errorf("pretendler not registered to GET / !")
-	}
-
-	RegisterHandler("GET", "/", altler)
-
-	code, _, _ = (*path_handlers[re_pointer])["GET"](blank)
-
-	if code != 400 {
-		test.Errorf("altler did not replace pretendler to GET / !")
-	}
-
-	stored_expressions = make(map[string]*regexp.Regexp)
-	path_handlers = make(map[*regexp.Regexp]*MethodMap)
-}
-
-func Test_RegisterMiddleware(test *testing.T) {
-	defer restore()
-
-	if len(middleware_handlers) != 0 {
-		test.Errorf("Something exists in middleware_handlers! %#v", middleware_handlers)
-	}
-
-	RegisterMiddleware(donotpass)
-
-	var pass bool
-	var code int
-	_, pass, code, _, _ = middleware_handlers[0](blank)
-
-	if pass {
-		test.Errorf("donotpass allowed us to pass!")
-	}
-
-	if code != 409 {
-		test.Errorf("donotpass returned status %d", code)
-	}
-}
-
-func Test_RegisterDefaultRoute(test *testing.T) {
-	defer restore()
-
-	RegisterDefaultRoute(defaultable)
-
-	var code int
-	if code, _, _ = default_route(blank); code != 300 {
-		test.Errorf("default_route got code %d", code)
-	}
-}
-
-func Test_RegisterDefaultMethod(test *testing.T) {
-	defer restore()
-
-	RegisterDefaultMethod(defaultable)
-
-	var code int
-	if code, _, _ = default_method(blank); code != 300 {
-		test.Errorf("default_method got code %d", code)
-	}
-}
-
-func Test_defaultRoute(test *testing.T) {
-	var code int
-	var r_map map[string]interface{}
-	if code, r_map, _ = defaultRoute(new(http.Request)); code != 404 {
-		test.Errorf("defaultRoute returned with code %d", code)
-	}
-
-	if r_map["error"].(string) != "not_found" {
-		test.Errorf("bad r_map %#v", r_map)
-	}
-}
-
-func Test_defaultMethod(test *testing.T) {
-	var code int
-	var r_map map[string]interface{}
-	if code, r_map, _ = defaultMethod(new(http.Request)); code != 405 {
-		test.Errorf("defaultMethod returned with code %d", code)
-	}
-
-	if r_map["error"].(string) != "bad_method" {
-		test.Errorf("bad r_map %#v", r_map)
-	}
-}
-
-func Test_resolveHandler(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^foobar/?$", pretendler)
-
-	var code int
-	if code, _, _ = resolveHandler("POST", "foobar")(blank); code != 200 {
-		test.Errorf("resolveHandler did not route to pretendler! got code %d", code)
-	}
-}
-
-func Test_resolveHandler_nil(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^foobar/?$", pretendler)
-	path_handlers[stored_expressions["^foobar/?$"]] = nil
-
-	var code int
-	if code, _, _ = resolveHandler("POST", "foobar")(blank); code != 404 {
-		test.Errorf("resolveHandler did not route to default_route! got code %d", code)
-	}
-}
-
-func Test_resolveHandler_badmethod(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^foobar/?$", pretendler)
-
-	var code int
-	if code, _, _ = resolveHandler("get", "foobar")(blank); code != 405 {
-		test.Errorf("resolveHandler did not route to default_method! got code %d", code)
-	}
-}
-
-func Test_resolveHandler_notfound(test *testing.T) {
-	var code int
-	if code, _, _ = resolveHandler("POST", "foobar")(blank); code != 404 {
-		test.Errorf("resolveHandler did not route to default_route! got code %d", code)
-	}
-}
-
-func Test_handleAfterMiddleware(test *testing.T) {
-	defer restore()
-	RegisterMiddleware(donotpass)
-	RegisterHandler("POST", "^foobar/?$", pretendler)
-
-	var code int
-	if code, _, _ = handleAfterMiddleware(blank, resolveHandler("POST", "foobar"), nil); code != 409 {
-		test.Errorf("call was not intercepted by donotpass! got code %d", code)
-	}
-}
-
-func Test_Route(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^/foobar/?$", pretendler)
-
-	var request *http.Request
-	var err error
-	if request, err = http.NewRequest("POST", "http://localhost/foobar/", nil); err != nil {
-		test.Fatal(err)
-	}
-
-	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
-	Route(recorder, request)
-
-	if recorder.Code != 200 {
-		test.Errorf("request was not routed to pretendler! got code %d", recorder.Code)
-	}
-}
-
-func Test_route_204(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^/foobar/?$", silent)
-
-	var request *http.Request
-	var err error
-	if request, err = http.NewRequest("POST", "http://localhost/foobar/", nil); err != nil {
-		test.Fatal(err)
-	}
-
-	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
-	Route(recorder, request)
-
-	if recorder.Code != 204 {
-		test.Errorf("request was not routed to silent! got code %d", recorder.Code)
-	}
-}
-
-func Test_route_badrmap(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^/foobar/?$", ohno)
-
-	var request *http.Request
-	var err error
-	if request, err = http.NewRequest("POST", "http://localhost/foobar/", nil); err != nil {
-		test.Fatal(err)
-	}
-
-	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
-	Route(recorder, request)
-
-	if recorder.Code != 500 {
-		test.Errorf("request was not routed to ohno! got code %d", recorder.Code)
-	}
-}
-
-func Test_route_err(test *testing.T) {
-	defer restore()
-	RegisterHandler("POST", "^/foobar/?$", ahshit)
-
-	var request *http.Request
-	var err error
-	if request, err = http.NewRequest("POST", "http://localhost/foobar/", nil); err != nil {
-		test.Fatal(err)
-	}
-
-	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
-	Route(recorder, request)
-
-	if recorder.Code != 500 {
-		test.Errorf("request was not routed to ahshit! got code %d", recorder.Code)
-	}
-}
-
-func Test_RegisterCatch(test *testing.T) {
-	defer restore()
-
-	RegisterCatch(400, map[string]interface{}{"error": "foobar"})
-	RegisterHandler("POST", "/", altler)
-
-	var request *http.Request
-	var err error
-	if request, err = http.NewRequest("POST", "http://localhost/", nil); err != nil {
-		test.Fatal(err)
-	}
-
-	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
-	Route(recorder, request)
-
-	var data []byte
-	if data, err = ioutil.ReadAll(recorder.Body); err != nil {
-		test.Fatal(err)
-	}
-
-	var fetched map[string]interface{}
-	if err = json.Unmarshal(data, &fetched); err != nil {
-		test.Fatal(err)
-	}
-
-	if fetched["error"].(string) != "foobar" {
-		test.Errorf("response mismatch! have: %s, want: foobar", fetched["error"])
-	}
 }
