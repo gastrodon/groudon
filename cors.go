9180f010338a14ab7e2a36ae6ffcfc086f46ae5e
Eva Harris <mail@gastrodon.io>
2021-04-08 23:05:00 -0400

    Add AllowOrigin
    Add AllowOrigin for adding origins to those that are allowed by this
    router

--- cors.go
+++ cors.go
@@ -4,6 +4,11 @@ var (
 	allowedOrigins map[string]bool = make(map[string]bool, 0)
 )
 
+func AllowOrigin(origin string) {
+	allowedOrigins[origin] = true
+	return
+}
+
 func allowOriginHeader(origin string) (header string) {
 	var ok, exists bool
 	if ok, exists = allowedOrigins[origin]; ok && exists {
