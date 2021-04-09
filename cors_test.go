1b2c57911c5c8d032de575021bcee7a4064e2f3f
Eva Harris <mail@gastrodon.io>
2021-04-09 01:02:31 -0400

    Add more cors tests

--- cors_test.go
+++ cors_test.go
@@ -38,3 +38,41 @@ func Test_handlePreflight_DisallowedOrigin(test *testing.T) {
 	recorderOk(recorder, code, nil, test)
 	corsOk(recorder, origin, test, false)
 }
+
+func Test_handlePreflight_methods(test *testing.T) {
+	test.Cleanup(restore)
+
+	AddHandler("GET", ".*", handlerSays(""))
+	AddHandler("PATCH", "^/foobar/?$", handlerSays(""))
+	AddHandler("POST", "^/unreachable/?$", handlerSays(""))
+
+	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
+	var code int = 204
+	handlePreflight(recorder, request("OPTIONS", "/foobar"))
+
+	recorderOk(recorder, code, nil, test)
+
+	var methods string = recorder.Header().Get("Access-Control-Allow-Methods")
+	if methods != "GET, PATCH" && methods != "PATCH, GET" {
+		test.Fatalf("Bad allowed methods: %s", methods)
+	}
+}
+
+func Test_handlePreflight_routed(test *testing.T) {
+	test.Cleanup(restore)
+
+	AddHandler("GET", ".*", handlerSays(""))
+	AddHandler("PATCH", "^/foobar/?$", handlerSays(""))
+	AddHandler("POST", "^/unreachable/?$", handlerSays(""))
+
+	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
+	var code int = 204
+	Route(recorder, request("OPTIONS", "/foobar"))
+
+	recorderOk(recorder, code, nil, test)
+
+	var methods string = recorder.Header().Get("Access-Control-Allow-Methods")
+	if methods != "GET, PATCH" && methods != "PATCH, GET" {
+		test.Fatalf("Bad allowed methods: %s", methods)
+	}
+}
