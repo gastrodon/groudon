77b5b8ab077f9827242a7c77da756d909e67a561
Eva Harris <mail@gastrodon.io>
2021-11-16 02:18:57 -0500

    Put the log nonce in the regular body

--- log.go
+++ log.go
@@ -10,13 +10,11 @@ func logRequest(request *http.Request, nonce string) {
 	logrus.WithFields(logrus.Fields{
 		"method": request.Method,
 		"path":   request.URL.Path,
-		"nonce":  nonce,
-	}).Info("")
+	}).Info(nonce)
 }
 
 func logResponse(code int, nonce string) {
 	logrus.WithFields(logrus.Fields{
-		"code":  code,
-		"nonce": nonce,
-	}).Info("")
+		"code": code,
+	}).Info(nonce)
 }
